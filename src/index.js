831c55ff2e545b0d1ede47e5c1cc6f4983fc886f
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-01-25 14:49:05 -0600

    added a card component object in react

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
 
 const App = () => {
     return (
+
         <div className="ui container comments">
+
+            <ApprovalCard/>
+
             <CommentDetail
                 author={"jon"}
                 timeAgo={"Today at 4:00PM"} text={"What time are you guys meeting up?"}
